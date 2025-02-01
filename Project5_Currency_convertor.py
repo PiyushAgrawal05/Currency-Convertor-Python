e8e6026386c95071f78767b080a86a239873819d
Piyush Agrawal <146958650+PiyushAgrawal05@users.noreply.github.com>
2025-02-01 16:12:04 +0530

    Project5_Currency_convertor.py

--- Project5_Currency_convertor.py
+++ Project5_Currency_convertor.py
@@ -4,17 +4,28 @@ import json
 
 # getting the base country and the final country
 base_country = input("Enter the base country you wish to get the exchange rate of:")
-country = countries_and_currencies[base_country.title()]
+try:
+    country = countries_and_currencies[base_country.title()]
+except KeyError:
+    print('Invalid country name')
+    exit()
 to_country = input("Enter the country you want to get the exchange value of from the base country:")
-final_country = countries_and_currencies[to_country.title()]
+try:
+    final_country = countries_and_currencies[to_country.title()]
+except KeyError:
+    print('Invalid country name')
+    exit()
 amount = int(input('Enter the amount you want to get the exchange value of:'))
 
 url = f'https://v6.exchangerate-api.com/v6/41c7f2f16235a7dd82e05159/latest/{country}'
 
-# getting request 
-api_request = requests.get(url)
-data = json.loads(api_request.text)
-
+# getting request
+try:
+    api_request = requests.get(url)
+    data = json.loads(api_request.text)
+except Exception as e:
+    print(f'An error occured: {e}')
+    
 # calculating the final amount
 final_amount = amount*data['conversion_rates'][final_country]
 print(f'{amount}{country} = {final_amount}{final_country}')
